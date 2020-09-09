d4cac231cd4df0f90eef4f95fc999b980e3bac90
Meklit Cherie <makywonde347@yahoo.com>
2020-09-08 21:02:38 -0700

    changes the file

---
+++ functions.py
@@ -0,0 +1,47 @@
+# Q1
+def area(width, height):
+    result = width * height
+    return result
+
+# Q2
+result = area(5, 6)
+# print(result)
+
+# Q3
+result= area(3,7)
+print (result)
+
+
+# Q4
+def subtract(num1, num2):
+    result= num1 - num2
+    return result
+
+
+# Q5
+result= subtract(4,2)
+print(result)  
+
+result= subtract(10,7)
+print(result) 
+
+# Q6
+def divide(income, spent):
+
+    result= income // spent
+    return result
+
+result= divide(1000, 500)
+print (result)
+
+# Q7
+def add(phone, utility):
+    result= phone + utility
+    return result 
+    
+result= add(50,150)
+print (result) 
+
+result= add(80,200)
+print (result) 
+
